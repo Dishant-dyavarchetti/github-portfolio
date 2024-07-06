ae3e117c1733fe7f5e5d0b827417d252b1a8519b
Dishant Dyavarchetti <136883310+Dishant-dyavarchetti@users.noreply.github.com>
2024-07-06 22:09:12 +0530

    Add files via upload

---
+++ practice_js.js
@@ -0,0 +1,31 @@
+var a=5;
+var b=11;
+var c=a+b;
+
+console.log(c);
+
+for(let i=1;i<=5;i++)
+{
+    console.log("Iteration" + i);
+}
+var inc=5;
+console.log(++inc);
+
+// array
+
+let fruits = ["apple","banana","Orange"];
+console.log(fruits);
+fruits.push("grape");
+fruits.push("Mango");
+fruits.pop();
+console.log(fruits);
+
+let person = {
+    name: "Dishant",
+    age: 25,
+    Dept: "CS"
+};
+console.log(person);
+
+const name =" dishant";
+console.log("hello, ${name}")
